f9de57781021bcc40d80ebb0f7ebdbd5026eeff9
CoraliaArguedasFeijoo <107568350+CoraliaArguedasFeijoo@users.noreply.github.com>
2023-04-17 21:15:04 -0600

    Update SearchCriteriaRepository.java

--- src/main/java/furrymatch/repository/SearchCriteriaRepository.java
+++ src/main/java/furrymatch/repository/SearchCriteriaRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 /**
  * Spring Data JPA repository for the SearchCriteria entity.
@@ -15,4 +16,7 @@ import org.springframework.stereotype.Repository;
 public interface SearchCriteriaRepository extends JpaRepository<SearchCriteria, Long> {
     @Query(value = "SELECT * FROM search_criteria WHERE pet_id = :id", nativeQuery = true)
     SearchCriteria findByPetId(@Param("id") Long id);
+    
+    @Query(value = "SELECT * FROM search_criteria WHERE pet_id = :id", nativeQuery = true)
+    Optional<SearchCriteria> findByOwnerUser(@Param("id") Long id);
 }
